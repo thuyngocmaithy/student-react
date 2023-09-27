d9199d832f7b03d6167d3a1d67d6eef81429511e
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2023-09-27 23:26:46 +0700

    frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TopBar from './components/TopBar.js'
+import Student from './components/Student.js'
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <Student />
+
+    </div>
+  );
+}
+
+export default App;
